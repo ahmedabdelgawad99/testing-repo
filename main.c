0326f70469ca1deb56182c11bae5646183bdafe4
ahmedabdelgawad99 <ahmedabdelgawad60@gmail.com>
2021-08-14 16:49:00 +0200

    hello github change

--- main.c
+++ main.c
@@ -3,9 +3,10 @@
 
 int main()
 {
-    /*this is new change*/
+    /*this is new change     */
     int x=5;
     int y=6;
     printf("Hello world!\n");
+    printf("hello github.");
     return 0;
 }
